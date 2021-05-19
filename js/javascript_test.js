25b5befe001bd2966be7a1067b9da6c60f58bb6e
yuka <uan1116@gmail.com>
2021-05-19 11:21:13 +0900

    re-first-commit

---
+++ js/javascript_test.js
@@ -0,0 +1,27 @@
+const array = [2, 4, 7, 5, 4, 3, 8];
+
+const result = array.filter(function(x, i, self) {
+  return self.indexOf(x) === i;
+});
+
+console.log(result);
+
+
+
+function isLeapYear(year){
+  if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
+
+let checkYear = 2020;
+if(isLeapYear(checkYear)){
+  console.log(checkYear + '年はうるう年');
+}else{
+  console.log(checkYear + '年はうるう年ではない');
+}
+
+
+
+
